0a833ba2ef449490785a39ec425636d9ce2d78d0
Jeremy Raines <jraines@gmail.com>
2015-09-27 16:52:47 -0400

    Draw gradients

--- src/noise/core.cljs
+++ src/noise/core.cljs
@@ -9,24 +9,59 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
-(defn foo []
+
+(def gradients
+  "test"
+  [[14 21]
+   [14 -21]
+   [21 14]
+   [-21 14]
+   [-21 -21]
+   [21 -21]
+   [21 21]
+   [-21 21]])
+
+(defn get-gradient [[a b]]
+  (let [ndx (int (mod (+ a b) 7))]
+    (get gradients ndx)))
+
+(defn xPlusGrad [x y]
+  (+ x (first (get-gradient [x y]))))
+
+(defn yPlusGrad [x y]
+  (+ y (second  (get-gradient [x y]))))
+
+(defn drawGrid []
   (let [canvas (.getElementById js/document "surface")
         ctx (.getContext canvas "2d")]
-    (set! (.-strokeStyle ctx) "black")
+    (set! (.-strokeStyle ctx) "blue")
     (.translate ctx 0.5 0.5)
-    (doseq [n (range 50 500 50)]
+    (doseq [n (range 100 500 100)]
         (.beginPath ctx)
-        (.moveTo ctx 0 n)
-        (.lineTo ctx 500 n)
+        (.moveTo ctx 100 n)
+        (.lineTo ctx 400 n)
         (.stroke ctx)
         (.beginPath ctx)
-        (.moveTo ctx n 0)
-        (.lineTo ctx n 500)
+        (.moveTo ctx n 100)
+        (.lineTo ctx n 400)
         (.stroke ctx)
-        ))
-  )
+        )))
 
-(foo)
+(defn drawGradients []
+  (let [canvas (.getElementById js/document "surface")
+        ctx (.getContext canvas "2d")]
+    (set! (.-strokeStyle ctx) "red")
+    (doseq [x (range 100 500 100)]
+      (doseq [y (range 100 500 100)]
+        (.beginPath ctx)
+        (.moveTo ctx x y)
+        (.lineTo ctx (xPlusGrad x y) (yPlusGrad x y))
+        (.stroke ctx)))))
+
+
+
+(drawGrid)
+(drawGradients)
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
