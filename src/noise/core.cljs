3c9e8afd6233313de1cc191f3a568db4dd250474
Jeremy Raines <jraines@gmail.com>
2015-09-27 18:23:58 -0400

    Draw point vectors

--- src/noise/core.cljs
+++ src/noise/core.cljs
@@ -58,10 +58,38 @@
         (.lineTo ctx (xPlusGrad x y) (yPlusGrad x y))
         (.stroke ctx)))))
 
+(defn drawPoint []
+  (let [canvas (.getElementById js/document "surface")
+        ctx (.getContext canvas "2d")]
+    (set! (.-strokeStyle ctx) "green")
+    (.fillRect ctx 221 139 3 3)))
 
+(defn get-corners [x y]
+  (let [nw [(- x (mod x 100)) (- y (mod y 100))]
+        [x y] nw
+        ne [(+ 100 x) y]
+        sw [x (+ 100 y)]
+        se [(+ 100 x) (+ 100 y)]]
+    [nw se sw se])
+  )
+
+(defn drawPointVectors []
+  (let [canvas (.getElementById js/document "surface")
+        ctx (.getContext canvas "2d")
+        x 221
+        y 139]
+    (set! (.-strokeStyle ctx) "pink")
+    (doseq [[cx cy] (get-corners x y)]
+      (.beginPath ctx)
+      (.moveTo ctx cx cy)
+      (.lineTo ctx x y)
+      (.stroke ctx)
+      )))
 
 (drawGrid)
 (drawGradients)
+(drawPoint)
+(drawPointVectors)
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
