673facd5fad87178ba52ce86357d51170ebc1ce6
Jeremy Raines <jraines@gmail.com>
2015-09-27 16:28:04 -0400

    Canvas with OK lines grid

--- src/noise/core.cljs
+++ src/noise/core.cljs
@@ -9,6 +9,24 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(defn foo []
+  (let [canvas (.getElementById js/document "surface")
+        ctx (.getContext canvas "2d")]
+    (set! (.-strokeStyle ctx) "black")
+    ;(.translate ctx 0.5 0.5)
+    (doseq [n (range 50.5 500 50)]
+        (.beginPath ctx)
+        (.moveTo ctx 0.5 n)
+        (.lineTo ctx 500.5 n)
+        (.stroke ctx)
+        (.beginPath ctx)
+        (.moveTo ctx n 0.5)
+        (.lineTo ctx n 500.5)
+        (.stroke ctx)
+        ))
+  )
+
+(foo)
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
